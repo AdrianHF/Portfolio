8a6e6f96689da97efe9d42bf448ac9e0722c831f
Adrian <felixadrianhdez@gmail.com>
2025-03-30 03:09:43 -0600

    Cleaning code (spaces, indentations, etc.)

--- js/shipPage.js
+++ js/shipPage.js
@@ -3,11 +3,11 @@ const tText = document.getElementById('transparentText');
 const button = document.getElementById('expandButton');
 
 button.addEventListener('click', () => {
-  if (!tText.classList.contains('visible')) {
-    tText.style.display = 'block'; // Make the element visible
-    setTimeout(() => tText.classList.add('visible'), 0); // Add transition effect after display update
-  } else {
-    tText.classList.remove('visible'); // Remove transition effect
-    setTimeout(() => tText.style.display = 'none', 500); // Hide the element after transition completes
-  }
+    if (!tText.classList.contains('visible')) {
+        tText.style.display = 'block'; // Make the element visible
+        setTimeout(() => tText.classList.add('visible'), 0); // Add transition effect after display update
+    } else {
+        tText.classList.remove('visible'); // Remove transition effect
+        setTimeout(() => tText.style.display = 'none', 500); // Hide the element after transition completes
+    }
 });
